4d1915d21a33ce1542d5078c6eee89ebd5b79b3e
masaharu <dabao456democebu3@gmail.com>
2022-10-17 02:41:49 +0900

    課題チャレンジ３

---
+++ MasaKadai3/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MasaKadai3
+//
+//  Created by Mina on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var leftNumberTextField: UITextField!
+    @IBOutlet private weak var leftSwitch: UISwitch!
+    @IBOutlet private weak var rightNumberTextField: UITextField!
+    @IBOutlet private weak var rightSwitch: UISwitch!
+    @IBOutlet private weak var leftNumberLabel: UILabel!
+    @IBOutlet private weak var rightNumberLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+
+    @IBAction private func resultButton(_ sender: Any) {
+        var leftNumber = Int(leftNumberTextField.text ?? "") ?? 0
+        var rightNumber = Int(rightNumberTextField.text ?? "") ?? 0
+
+        if leftSwitch.isOn == true {
+            leftNumberLabel.text = "-" + String(leftNumber)
+            leftNumber *= -1
+        } else {
+            leftNumberLabel.text = String(leftNumber)
+        }
+
+        if rightSwitch.isOn == true {
+            rightNumberLabel.text = "-" + String(rightNumber)
+            rightNumber *= -1
+        } else {
+            rightNumberLabel.text = String(rightNumber)
+        }
+
+        resultLabel.text = String(leftNumber + rightNumber)
+    }
+
+}
